f6b27753b3562a29f3ef6d7b0099f5e93b9bc686
Munsif-UT <85740319+Munsif-UT@users.noreply.github.com>
2021-07-19 10:48:01 -0700

    searching of products done

--- front-end/src/Screens/AllProducts/AllProducts.jsx
+++ front-end/src/Screens/AllProducts/AllProducts.jsx
@@ -1,34 +1,112 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Drawer, Button } from "antd";
 import CallReceivedIcon from "@material-ui/icons/CallReceived";
 import NestedDrawer from "./Drawer";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import SearchIcon from "@material-ui/icons/Search";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 function AllProducts() {
   const [visible, setvisible] = useState(false);
   const [childDrawer, setChildDrawer] = useState();
   const [SearchFiled, setSearchFiled] = useState("");
+  const [Products, setProducts] = useState([]);
 
   // state = { visible: false, childrenDrawer: false };
+  async function fetchProducts() {
+    const params = new URLSearchParams(window.location.search);
+    const page = params.get("page");
+    const limit = params.get("limit");
+    console.log(page + " " + limit);
+    if (SearchFiled) {
+      console.log(SearchFiled);
+      const { data } = await axios.get(
+        `http://localhost:8000/?name[regex]=${SearchFiled}&name[options]=i&page=${params.get(
+          "page"
+        )}&limit=${params.get("limit")}`
+      );
+      console.log(data);
+      setProducts(data);
+    } else {
+      const { data } = await axios.get(`http://localhost:8000/`);
+      setProducts(data);
+    }
+  }
+
+  useEffect(() => {
+    //create an axios fetch request to get the
+    fetchProducts();
+  }, [SearchFiled]);
+  return (
+    <div className="all_product container">
+      <div className="Searchbarwrapper row">
+        <div className="searchBar col-xs-12 col-lg-12 col-md-12">
+          <form action="">
+            <SearchIcon className="icons" />
+            <input
+              type="text"
+              name="name"
+              id="searchProduct"
+              placeholder="Search for Product"
+              value={SearchFiled}
+              autoFocus="true"
+              onChange={(e) => setSearchFiled(e.target.value)}
+            />
+          </form>
+        </div>
+      </div>
+      <div className="row Products">
+        {console.log(Products)}
+        {Products.length === 0 ? (
+          <h1>Soory no product available</h1>
+        ) : (
+          Products.map((prod, index) => (
+            <div className="col-md-12 singleProd">
+              <div key={index} className={`product${index + 1} commonProduct`}>
+                <div className="imgBox">
+                  <img src={prod.imgUrl} alt={prod.name} />
+                </div>
+                <div className="infoBoxWrapper">
+                  <div className="infoBox">
+                    <div className="Prodname">
+                      <p>{prod.name}</p>
+                      <span>Count In Stock: {prod.countInStock}</span>
+                    </div>
+                    <div className="price">
+                      <span className="price-after">{prod.price}</span>
+                      <span className="price-before"> Rs.</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default AllProducts;
+
+/* 
 
   function showDrawer() {
     setvisible(true);
   }
-
   function onClose() {
     setvisible(false);
   }
   function showChildDrawer() {
     setChildDrawer(true);
   }
-
   function closeChildDrawer() {
     setChildDrawer(false);
-  }
-
-  return (
-    <div className="all_product container">
-      {/* <div className="filterArea">
+  } 
+  
+  
+  
+  <div className="filterArea">
         <Button type="primary" onClick={showDrawer}>
           Filter & Sort
         </Button>
@@ -56,31 +134,5 @@ function AllProducts() {
             <CallReceivedIcon />
           </div>
         </Drawer>
-      </div> */}
-      <div className="mainContent row">
-        <div className="searchBar col">
-          <form action="">
-            <SearchIcon />
-            <input
-              type="text"
-              name="searchProduct"
-              id="searchProduct"
-              placeholder="Search"
-              value={SearchFiled}
-              onChange={(e) => setSearchFiled(e.target.value)}
-            />
-            <input
-              type="submit"
-              value="Submit"
-              onClick={(e) => {
-                e.preventDefault();
-              }}
-            />
-          </form>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-export default AllProducts;
+      </div> 
+  */
