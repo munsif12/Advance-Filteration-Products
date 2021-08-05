c33ea4974dc656e42d247c4e024606efc7fb7c3a
munsif12 <munsifalimisri69@gmail.com>
2021-08-06 03:11:52 +0500

    main app.js file

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,18 @@
 import AllProducts from "./Screens/Homepage/AllProducts";
 import "./App.css";
-
+import backgroundImg from "./bgImg.png";
+import { Switch, Route } from "react-router-dom";
+import ProductDetail from "./Screens/ProductDetail/ProductDetail";
 function App() {
   return (
-    <div className="App">
-      <AllProducts />
+    <div
+      className="App"
+      style={{ background: `Url(${backgroundImg})`, backgroundSize: "cover" }}
+    >
+      <Switch>
+        <Route exact path="/" component={AllProducts} />
+        <Route path="/product/:id" component={ProductDetail} />
+      </Switch>
     </div>
   );
 }
