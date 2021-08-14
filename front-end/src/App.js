e15c80dbe903540b7b220602374b6a22a20cf027
munsif12 <munsifalimisri69@gmail.com>
2021-08-14 17:31:32 +0500

    navbar added

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,12 +4,14 @@ import backgroundImg from "./bgImg.png";
 import { Switch, Route } from "react-router-dom";
 import ProductDetail from "./Screens/ProductDetail/ProductDetail";
 import WishlistedProducts from "./Screens/Wislist-Products/WishlistedProducts";
+import Navbar from "./components/Navbar/Navbar";
 function App() {
   return (
     <div
       className="App"
       style={{ background: `Url(${backgroundImg})`, backgroundSize: "cover" }}
     >
+      <Navbar />
       <Switch>
         <Route exact path="/" component={AllProducts} />
         <Route exact path="/product/:id" component={ProductDetail} />
